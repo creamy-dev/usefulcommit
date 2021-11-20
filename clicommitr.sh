69a1a3c6313a3390870391219c2526871edb6322
creamy-dev <56458201+creamy-dev@users.noreply.github.com>
2021-11-20 18:49:38 -0500

    feature: Adds PATH adding.

--- clicommitr.sh
+++ clicommitr.sh
@@ -1,5 +1,10 @@
 #!/bin/bash 
+echo "WARNING: If you are using ordinary clicommitr in the path, it WILL break!"
+echo "Instead, use clicommitr_diversion.sh."
+export URL=$PWD
+cd "magic"
 MAGIC=$(node index.js --commit="$1" --type="$2" --lint)
+cd $URL
 echo "Commiting '$MAGIC'... Press Ctrl+C to cancel."
 sleep 1s
 echo "Continuing."
