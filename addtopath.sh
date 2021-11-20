e205d3ac94d0248be408722b25f4272e344ec529
creamy-dev <56458201+creamy-dev@users.noreply.github.com>
2021-11-20 18:54:54 -0500

    fix: Patches Path not working correctly.

--- addtopath.sh
+++ addtopath.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Adding to path... Please wait..."
-echo export PATH='$PATH:$PWD' >> ~/.zshrc
-echo export PATH='$PATH:$PWD' >> ~/.bashrc
+echo "export PATH='\$PATH:$PWD'" >> ~/.zshrc
+echo "export PATH='\$PATH:$PWD'" >> ~/.bashrc
 cp clicommitr.sh clicommitr_diversion.sh
 sed -i "s/magic/$\(echo '$(echo $PWD | base64)' | base64 --decode\)/g" clicommitr_diversion.sh
