142034eaaf9b0ae867c0dd6b89af192430e1b836
creamy-dev <56458201+creamy-dev@users.noreply.github.com>
2021-11-20 19:09:47 -0500

    fix: Fix path for reals.

--- addtopath.sh
+++ addtopath.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Adding to path... Please wait..."
-echo "export PATH='\$PATH:$PWD'" >> ~/.zshrc
-echo "export PATH='\$PATH:$PWD'" >> ~/.bashrc
-cp commithelper.sh clicommitr
+echo ";export PATH=$PATH:$PWD" >> ~/.zshrc
+echo ";export PATH=$PATH:$PWD" >> ~/.bashrc
+cp com-mithelper.sh clicommitr
 chmod +x clicommitr
 sed -i "s/magic/$\(echo '$(echo $PWD | base64)' | base64 --decode\)/g" clicommitr
